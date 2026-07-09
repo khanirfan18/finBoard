9b40f7a6ae1e655366fc77b39fd695aff2cce098
Niranjan R Soorej <niranjansoorej24bsc0117@iiitkottayam.ac.in@gmail.com>
2026-07-10 00:28:23 +0530

    Titlebar icon fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,36 @@ import App from './App.jsx'
 import "@fontsource/inter";
 import { applyThemeToDocument, getStoredTheme } from './context/ThemeContext';
 
+const frames = [
+  "/favicon/frame_00.png",
+  "/favicon/frame_02.png",
+  "/favicon/frame_04.png",
+  "/favicon/frame_06.png",
+  "/favicon/frame_08.png",
+  "/favicon/frame_10.png",
+  "/favicon/frame_12.png",
+  "/favicon/frame_14.png",
+  "/favicon/frame_16.png",
+  "/favicon/frame_18.png",
+  "/favicon/frame_20.png",
+  "/favicon/frame_22.png",
+  "/favicon/frame_24.png",
+  "/favicon/frame_26.png",
+  "/favicon/frame_28.png",
+  "/favicon/frame_30.png",
+];
+
+let i = 0;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const favicon = document.getElementById("favicon");
+
+  setInterval(() => {
+    favicon.href = frames[i] + "?v=" + i; // helps avoid caching
+    i = (i + 1) % frames.length;
+  }, 100);
+});
+
 let bootTheme = 'dark';
 
 try {
